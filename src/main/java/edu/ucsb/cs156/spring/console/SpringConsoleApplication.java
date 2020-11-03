854de8fe075946b2aaae14092f0668df0f6dd682
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-11-03 12:08:19 -0800

    pc - initial pom.xml and java file for Spring Boot Console App Demo

---
+++ src/main/java/edu/ucsb/cs156/spring/console/SpringConsoleApplication.java
@@ -0,0 +1,33 @@
+package edu.ucsb.cs156.spring.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringConsoleApplication implements CommandLineRunner  {
+
+	private static Logger LOG = LoggerFactory.getLogger(SpringConsoleApplication.class);
+
+    public static void main(String [] args) {
+        LOG.info("STARTING THE APPLICATION");
+		SpringApplication.run(SpringConsoleApplication.class, args);
+		LOG.info("APPLICATION FINISHED");
+    }
+
+    /**
+	 * This method will be executed after the application context is loaded and
+	 * right before the Spring Application main method is completed.
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("EXECUTING : command line runner");
+    		for (int i = 0; i < args.length; ++i) {
+        		LOG.info("args[{}]: {}", i, args[i]);
+    		}
+	}
+
+}
